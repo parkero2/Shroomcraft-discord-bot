d24f33ad546070f035dbae60af946b172b9cccc6
parkero2 <parkero2@stu.otc.school.nz>
2021-09-17 10:50:42 +1200

    fixed cookie trading

--- commands/give.js
+++ commands/give.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const playerdata = require('../src/playerdata.json');
+const bag = require('./inventory.js');
 
 module.exports = {
     'name' : 'give',
@@ -7,8 +8,37 @@ module.exports = {
     'description' : 'Help command',
     execute(message) {
         let payload = message.content.split(" ");
-        if (message.mentions.members.first()) {
-            message.channel.send("Trade with yo mom");
+        if (message.mentions.members.first() && payload[1].includes(message.mentions.members.first().id)) {
+            if (playerdata[message.author.id]) {
+                try {
+                    if (playerdata[message.author.id].assets[payload[2].toLowerCase()] >= parseInt(payload[3])) {
+                        playerdata[message.author.id].assets[payload[2].toLowerCase()] -= parseInt(payload[3]);
+                        playerdata[message.mentions.members.first().id].assets[payload[2].toLowerCase()] += parseInt(payload[3]);
+                    }
+                    else {
+                        message.channel.send("You either have insufficient funds or the requested item does not exist.");
+                    }
+                    fs.writeFile('./src/playerdata.json', JSON.stringify(playerdata, null, 2), function (err) {
+                        if (err) {
+                            message.channel.send(`❌ Could not complete transaction.`);
+                            console.log(err)
+                            return false;
+                        }
+                        else {
+                            message.channel.send(`✅ Transaction completed succssfully.`);
+                            return true;
+                        }
+                    });
+                }
+                catch (err) {
+                    if (err) throw err;
+                    message.channel.send("An error occoured");
+                }
+            }
+            else {
+                bag.execute(message);
+                this.execute(message);
+            }
         }
         else {
             message.channel.send("Please mention the player you want to gift items to after the `" + payload[0] + "`");
